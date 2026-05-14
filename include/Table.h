16f04d6f5612406e3b5634c7f7f51baaee8592fe
dmitriimotuzov <dimamotuzov07@gmail.com>
2026-05-14 14:55:26 +0300

    delete function declarations

--- include/Table.h
+++ include/Table.h
@@ -20,16 +20,11 @@ typedef struct {
 } Table;
 
 void table_destroy(Table *t);
-int table_add_column(Table *t, const char *name);
 Row *table_add_row(Table *t, Row *row);
-Cell *table_get_cell(Table *t, int col_index, long row_num);
-int table_find_column_index(const Table *t, const char *name);
 
 Cell *get_cell(Table *t, CellRef ref);
 
 int evaluate_all(Table *t);
 int evaluate_cell(Table *t, Cell *cell);
-long evaluate_arg(Table *t, Arg *arg, int *ok);
 
 void table_print(const Table *t, FILE *f);
-void cell_print(const Cell *cell, FILE *f);
